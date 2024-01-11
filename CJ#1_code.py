eabe8cacee35c3ce4601b641159551481595976d
SamW <118303279+ZzzanQIE@users.noreply.github.com>
2024-01-11 14:04:44 -0800

    Create CJ#1_code.py

---
+++ CJ#1_code.py
@@ -0,0 +1,4 @@
+print('Full name: Ziling Wang')
+print('Pefer name: Sam Wang')
+print("My favorite movie is 'Weathering With You', the director is Shinkai Makoto")
+print('My facorite food is Chinese fried pork with chili peppers')
