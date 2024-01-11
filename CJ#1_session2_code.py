6f73554b5c1f477be0a4dc522fdae87a76dfe803
SamW <118303279+ZzzanQIE@users.noreply.github.com>
2024-01-11 14:15:00 -0800

    Add files via upload

---
+++ CJ#1_session2_code.py
@@ -0,0 +1,12 @@
+float_1 = 2.33
+float_2 = 3.44
+int_1 = 2
+int_2 = 3
+
+num_1 = float_1 + float_2
+num_2 = int_1 - int_2
+num_3 = float_1 * int_1
+
+print(num_1, 'the type of this number is:', type(num_1))
+print(num_2, 'the type of this number is:', type(num_2))
+print(num_3, 'the type of this number is:', type(num_3))
